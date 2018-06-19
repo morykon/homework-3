bf663bcefdf0e9d15f5c314d7d85b18126454dbd
morykon <38287617+morykon@users.noreply.github.com>
2018-06-19 03:12:14 +0300

    Update count_holes(n).py

--- count_holes(n).py
+++ count_holes(n).py
@@ -10,15 +10,23 @@ def count_holes(n):
         if p in m:
             print('ERROR')
             quit()
-        else:    
-            num = ('4', '6', '9', '0')
-            b = '8'
-            res = 0
-            for a in num:
-                if a in m:
-                    res += 1
-            if b in m:
-                res += 2
-
-            return res
-
+            
+    b = '8'
+    u = '4'
+    h = '6'
+    r = '9'
+    q = '0'
+    res = 0
+    for t in m:
+        if u in t:
+            res += 1
+        if h in t:
+            res += 1         
+        if b in t:
+            res += 2
+        if q in t:
+            res += 1 
+        if r in t:
+            res += 1             
+        
+    return res
